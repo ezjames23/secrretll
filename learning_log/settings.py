c9187fb1c1648527eb1c6772966ef1fe639422bf
ezjames23 <macabre310@gmail.com>
2021-02-01 14:53:18 +0800

    test commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -151,8 +151,8 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # static asset configuration
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    #STATIC_ROOT = 'staticfiles'
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
     #STATICFILES_DIRS = (
-     #   os.path.join('BASE_DIR', 'static'),
-      #  )
+    #    os.path.join('BASE_DIR', 'static'),
+    #    )
